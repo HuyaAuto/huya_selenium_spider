10ffd342108371552fdd8b6f68bd78217a349a5b
xiong3134 <752495329@qq.com>
2019-04-07 21:01:22 +0800

    first commit

--- douyu _spider.py
+++ douyu _spider.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
+import json
 import time
 class HuyaSpider:
     def __init__(self):
        self.url="https://www.huya.com/l"
        self.driver = webdriver.Chrome()
+
+
     def get_content_list(self):
         li_list=self.driver.find_elements_by_xpath(".//div[@class='box-bd']/ul/li")
         content_list=[]
@@ -16,23 +19,27 @@ class HuyaSpider:
             item['category']=li.find_element_by_xpath(".//span[contains(@class,'game-type')]/a").text
             content_list.append(item)
             print(item)
-        next_page =self.driver.find_element_by_xpath(".//a[@class='laypage_next']")
-        next_url = next_page[0] if len(next_page) >0 else None
+        next_url =self.driver.find_element_by_xpath(".//a[@class='laypage_next']")
+
 
 
 
         return content_list,next_url
-    def save_content_list(self):
-        pass
+    def save_content_list(self,content_list):
+        with open('test.json', 'w', encoding='utf-8') as fp:
+            json.dump(content_list, fp,ensure_ascii=False)
+
+
 
     def run(self):
         self.driver.get(self.url)
         content_list,next_url=self.get_content_list()
+        self.save_content_list(content_list)
         while next_url is not None:
             next_url.click()
             time.sleep(3)
             content_list,next_url=self.get_content_list()
-            self.save_content_list()
+            self.save_content_list(content_list)
 
 if __name__ =="__main__":
     huya = HuyaSpider()
